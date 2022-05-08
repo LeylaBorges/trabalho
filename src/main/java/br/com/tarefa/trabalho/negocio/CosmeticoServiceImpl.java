77c721e7b9201730899242a3457029d9cfa4e7c4
LeylaBorges <leylamalta@hotmail.com>
2022-05-08 16:10:47 -0300

    camada de negocio

--- src/main/java/br/com/tarefa/trabalho/negocio/CosmeticoServiceImpl.java
+++ src/main/java/br/com/tarefa/trabalho/negocio/CosmeticoServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.tarefa.trabalho.negocio;
 
 import br.com.tarefa.trabalho.entidade.Cosmetico;
 import br.com.tarefa.trabalho.persistencia.CosmeticoRepository;
+import br.com.tarefa.trabalho.util.TrabalhoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,62 +21,60 @@ public class CosmeticoServiceImpl implements CosmeticoService{
         this.repository = repository;
     }
 
+
     @Override
-    public Cosmetico incluir(Cosmetico cosmetico){
+    public Cosmetico incluir(Cosmetico cosmetico) {
         try {
             repository.save(cosmetico);
             return cosmetico;
         }
-        // catch (Exception e){
-            throw new TrabalhoException(Não foi possivel incluir o cosmetico)
+        catch (Exception e){
+            throw new TrabalhoException("Não foi possivel incluir o cosmetico");
         }
     }
+
     @Override
-    public List<Cosmetico>listar(){
-    try {
-        return (List<Cosmetico>) this.repository.findAll();
+    public List<Cosmetico> listar() {
+        try {
+            return (List<Cosmetico>) this.repository.findAll();
+        }
+        catch (Exception e){
+            throw new TrabalhoException ("Não foi possivel listar o cosmetico");
+        }
     }
-    catch (Exception e){
-        throw new TarefaException ("Não foi possivel listar os cosmeticos");
-    }
-}
+
     @Override
-    public Cosmetico consultar(Integer id);
+    public Cosmetico consultar(Integer id) {
+        try {
+            return this.repository.findById(id).get();
+
+        }
+        catch (Exception e){
+            throw new TrabalhoException("Não foi possivel consulta o cosmetico");
+        }
+    }
 
     @Override
     public void alterar(Cosmetico cosmetico) {
+        try{
+            this.repository.save(cosmetico);
+        }
+        catch (Exception e){
+            throw new TrabalhoException("Não foi possivel alterar o cosmetico");
+        }
 
     }
 
     @Override
     public void exluir(Integer id) {
-
-    }{
         try{
-        return this.repository.findById(id).get();
-        }
-        catch(Exception e){
-        throw new Trabalho3Exception("Não foi possivel consultar o cosmetico:"+id+"!");
-        }
-        }
-
-        @Override
-        public void alterar(Cosmetico cosmetico) {
-        try{
-            repository.save(cosmetico);
+            this.repository.deleteById(id);
         }
         catch (Exception e){
-            throw new TrabalhoException("Não foi possivel alterar o cosmetico: "+cosmetico.gerId()+"!");
-        }
+            throw new TrabalhoException("Não foi possivel alterar o cosmetico");
         }
 
-        @Override
-        public void excluir(Integer id){
-        try{
-            repository.deleteById(id);
-        }
-        catch (Exception e){
-            throw new TrabalhoException("Não foi possivel excluir o cosmetico: "+id+"!");
-        }
-        }
+    }
+}
+
 
