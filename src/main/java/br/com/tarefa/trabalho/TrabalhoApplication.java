dfa1e697f3456bb46f10af9793be6a5404f8d71f
LeylaBorges <leylamalta@hotmail.com>
2022-05-08 16:35:51 -0300

    application.properties, adicionei configurações

--- src/main/java/br/com/tarefa/trabalho/TrabalhoApplication.java
+++ src/main/java/br/com/tarefa/trabalho/TrabalhoApplication.java
@@ -8,13 +8,7 @@ public class TrabalhoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoApplication.class, args);
-		server.error.include-message=always
-		spring.datasource.driver-class-name=org.h2.Driver
-		spring.datasource.url=jdbc:h2:file:./database/TrabalhoApplication
-		spring.datasource.username=sa
-		spring.datasource.password=sa
-		spring.jpa.show-sql=true
-		spring.jpa.hibernate.ddl-auto=create
-				#spring.jpa.hibernate.ddl-auto=update
+	}
+
 
 }
--- src/main/java/br/com/tarefa/trabalho/util/TrabalhoException.java
+++ src/main/java/br/com/tarefa/trabalho/util/TrabalhoException.java
@@ -1,9 +1,10 @@
 package br.com.tarefa.trabalho.util;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class TrabalhoException extends ResponseStatusException{
-    public TrabalhoException(String mensagem){
-        super(HttpStatus.INTERNAL_SERVER_ERROR,mensagem);
+public class TrabalhoException extends ResponseStatusException {
+    public TrabalhoException(String msg){
+        super(HttpStatus.INTERNAL_SERVER_ERROR,msg);
     }
 }
